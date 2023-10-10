9423467ec037a6db1a4e440465f22b18f5c35eba
Guy Jara <guy_jara@yahoo.com>
2023-10-09 18:01:29 -0700

    Day 2 complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from .models import Finch
 
-finches = [
-  {'name': 'Tweety Bird', 'breed': 'yellow canary', 'description': 'too smart and cute for his own good', 'age': 3},
-  {'name': 'Precious', 'breed': 'chaffinch', 'description': 'cute song bird, with a relaxing and gentle song', 'age': 2},
-]
+# finches = [
+#   {'name': 'Tweety Bird', 'breed': 'yellow canary', 'description': 'too smart and cute for his own good', 'age': 3},
+#   {'name': 'Precious', 'breed': 'chaffinch', 'description': 'cute song bird, with a relaxing and gentle song', 'age': 2},
+# ]
 
 # Create your views here.
 def home(request):
@@ -15,6 +16,13 @@ def about(request):
 
 def finches_index(request):
   # We pass data to a template very much like we did in Express!
-  return render(request, 'finches/index.html', {
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', {    
     'finches': finches
-  })
+})
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+       'finch': finch
+    })
